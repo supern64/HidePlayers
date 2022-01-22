adda93ba446ca4192ba6ca7067b5cafba5de473a
supern64 <chanon-tong@hotmail.com>
2022-01-22 23:49:45 +0700

    unhide NPCs, add support for armorstand nametags

--- src/main/java/me/sad/hideplayers/HidePlayers.java
+++ src/main/java/me/sad/hideplayers/HidePlayers.java
@@ -9,12 +9,16 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @Mod(modid = HidePlayers.MOD_ID)
 public class HidePlayers {
     public static String prefix = "\u00a75HidePlayers \u00a7c- \u00a7r";
     public static final String MOD_ID = "hideplayers";
     public static List<String> players = new ArrayList<>();
+    public static Map<UUID, String> armorStandCache = new HashMap<>();
     public static boolean toggled = true;
 
     @Mod.EventHandler
--- src/main/java/me/sad/hideplayers/mixins/MixinRenderManager.java
+++ src/main/java/me/sad/hideplayers/mixins/MixinRenderManager.java
@@ -1,10 +1,18 @@
 package me.sad.hideplayers.mixins;
 
 import me.sad.hideplayers.HidePlayers;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityOtherPlayerMP;
 import net.minecraft.client.renderer.culling.ICamera;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.network.NetHandlerPlayClient;
+import net.minecraft.client.network.NetworkPlayerInfo;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityArmorStand;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,10 +22,31 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class MixinRenderManager {
     @Inject(at=@At("HEAD"), method = "shouldRender", cancellable = true)
     public void shouldRender(Entity entityIn, ICamera camera, double camX, double camY, double camZ, CallbackInfoReturnable<Boolean> cir) {
-        if (entityIn instanceof EntityOtherPlayerMP) {
-            if (!HidePlayers.players.contains(entityIn.getName().toLowerCase()) && !HidePlayers.toggled) {
+        Minecraft minecraft = Minecraft.getMinecraft();
+        NetHandlerPlayClient connection = minecraft.getNetHandler();
+
+        if (entityIn instanceof EntityOtherPlayerMP && !HidePlayers.toggled) {
+            if (entityIn.getName().matches("^[a-zA-Z0-9_]*$") && !HidePlayers.players.contains(entityIn.getName().toLowerCase())) {
                 cir.setReturnValue(false);
             }
         }
+        if (entityIn instanceof EntityArmorStand) {
+            if (!HidePlayers.armorStandCache.containsKey(entityIn.getPersistentID())) {
+                HidePlayers.armorStandCache.put(entityIn.getPersistentID(), null);
+                if (entityIn.hasCustomName() && !entityIn.getCustomNameTag().toLowerCase().contains(minecraft.thePlayer.getName().toLowerCase())) {
+                    List<String> playerList = connection.getPlayerInfoMap().stream().map(NetworkPlayerInfo::getGameProfile).map(gameProfile -> gameProfile.getName()).collect(Collectors.toList());
+                    playerList.forEach(p -> {
+                        if (entityIn.getCustomNameTag().toLowerCase().contains(p.toLowerCase())) {
+                            HidePlayers.armorStandCache.put(entityIn.getPersistentID(), p);
+                        }
+                    });
+                }
+            }
+            String armorStandOwner = HidePlayers.armorStandCache.get(entityIn.getPersistentID());
+            if (armorStandOwner != null && !HidePlayers.players.contains(armorStandOwner.toLowerCase()) && !HidePlayers.toggled) {
+                cir.setReturnValue(false);
+            }
+        }
+        
     }
 }
